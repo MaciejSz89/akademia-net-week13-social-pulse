58ccb8a859e55b121eef44ebfb72f3dd04c50b74
Maciej Szafranski <maciej.szafranski1@gmail.com>
2024-12-10 22:43:14 +0100

    Scaffold items for identity added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SocialPulse.Areas.Identity.Data;
+using SocialPulse.Persistence;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SocialPulseContextConnection") ?? throw new InvalidOperationException("Connection string 'SocialPulseContextConnection' not found.");
+
+builder.Services.AddDbContext<SocialPulseContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SocialPulseUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<SocialPulseContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
