8cf3254679e5d2a0669606bd5e5d146ee06a22ab
Maciej Szafranski <maciej.szafranski1@gmail.com>
2025-01-06 17:56:00 +0100

    first commit

---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialPulse.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
