4a0f32506f841f4a7a08dee4f8075ce1dbf66f25
Maciej Szafranski <maciej.szafranski1@gmail.com>
2025-01-18 23:22:48 +0100

    Almost...

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -4,93 +4,133 @@ using SocialPulse.Core.Dtos;
 using SocialPulse.Core.Services;
 using SocialPulse.Core.ViewModels;
 
-namespace SocialPulse.Controllers
+namespace SocialPulse.Controllers;
+
+public class SettingsController : Controller
 {
-    public class SettingsController : Controller
+    private readonly ISocialNetworkService _socialNetworkService;
+    private readonly IMapper _mapper;
+    private readonly IViewRenderService _viewRenderService;
+
+    public SettingsController(IServiceProvider serviceProvider)
     {
-        private readonly ISocialNetworkService _socialNetworkService;
-        private readonly IMapper _mapper;
-        private readonly IViewRenderService _viewRenderService;
+        _socialNetworkService = serviceProvider.GetRequiredService<ISocialNetworkService>();
+        _mapper = serviceProvider.GetRequiredService<IMapper>();
+        _viewRenderService = serviceProvider.GetRequiredService<IViewRenderService>();
+    }
 
-        public SettingsController(IServiceProvider serviceProvider)
+    public async Task<IActionResult> Profile()
+    {
+        var socialNetworks = _mapper.Map<List<SocialNetworkViewModel>>(await _socialNetworkService.GetAsync());
+        List<SocialLinkViewModel> links = new List<SocialLinkViewModel>();
+
+        foreach (var socialNetwork in socialNetworks)
         {
-            _socialNetworkService = serviceProvider.GetRequiredService<ISocialNetworkService>();
-            _mapper = serviceProvider.GetRequiredService<IMapper>();
-            _viewRenderService = serviceProvider.GetRequiredService<IViewRenderService>();
+            links.Add(new SocialLinkViewModel
+            {
+                SocialNetwork = socialNetwork,
+            });
         }
 
-        public async Task<IActionResult> Profile()
+        var vm = new SocialProfileViewModel
         {
-            var socialNetworks = _mapper.Map<List<SocialNetworkViewModel>>(await _socialNetworkService.GetAsync());
-            List<SocialLinkViewModel> links = new List<SocialLinkViewModel>();
+            Email = "test@test.com",
+            UserName = "user",
+            SocialLinks = links
+        };
 
-            foreach (var socialNetwork in socialNetworks)
+        return View(vm);
+    }
+    public IActionResult MyLinks()
+    {
+        var model = new SocialProfileViewModel
+        {
+            UserName = "John Doe",
+            UserLinks = new List<UserLinkViewModel>
+                        {
+                            new UserLinkViewModel { 
+                                                    Id = 1, 
+                                                    Title = "Facebook", 
+                                                    Url = "https://facebook.com/johndoe", 
+                                                    Image = GetSampleImage("facebook.png") 
+                                                  },
+                            new UserLinkViewModel { 
+                                                    Id = 2, 
+                                                    Title = "X", 
+                                                    Url = "https://x.com/johndoe" ,
+                                                    Image = GetSampleImage("x.png")
+                                                  }
+                        }
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult RemoveUserLink(int Id)
+    {
+
+        if (!ModelState.IsValid)
+        {
+            return Json(new ResponseDto
             {
-                links.Add(new SocialLinkViewModel
-                {
-                    SocialNetwork = socialNetwork,
-                });
+                IsSuccess = false,
+                Message = "Invalid input data."
+            });
+        }
+
+        var response = new ResponseDto
+        {
+            IsSuccess = true,
+        };
+
+        return Json(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddUserLink([FromForm] UserLinkDto newLinkDto, IFormFile image)
+    {
+
+        if (!ModelState.IsValid)
+        {
+            return Json(new ResponseDto
+            {
+                IsSuccess = false,
+                Message = "Invalid input data."
+            });
+        }
+
+        var newLink = _mapper.Map<UserLinkViewModel>(newLinkDto);
+        newLink.Id = new Random().Next(1, 1000);
+
+        if (image != null)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                var fileBytes = ms.ToArray();
+                newLink.Image = fileBytes;
             }
-
-            var vm = new SocialProfileViewModel
-            {
-                Email = "test@test.com",
-                UserName = "user",
-                SocialLinks = links
-            };
-
-            return View(vm);
         }
-        public IActionResult MyLinks()
+
+        var response = new ResponseDto<string>
         {
-            var model = new SocialProfileViewModel
-            {
-                UserName = "John Doe",
-                UserLinks = new List<UserLinkViewModel>
-                            {
-                                new UserLinkViewModel { Id = 1, Title = "Facebook", Url = "https://facebook.com/johndoe" },
-                                new UserLinkViewModel { Id = 2, Title = "Twitter", Url = "https://twitter.com/johndoe" }
-                            }
-            };
+            IsSuccess = true,
+            Data = await _viewRenderService.RenderToStringAsync("_UserLinkRow", newLink)
+        };
 
-            return View(model);
-        }
+        return Json(response);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> AddUserLink([FromForm] UserLinkDto newLinkDto, IFormFile image)
+    private byte[]? GetSampleImage(string imageName)
+    {
+        var filePath = Path.Combine("wwwroot", "images", imageName);
+        if (!System.IO.File.Exists(filePath))
         {
-
-            if (!ModelState.IsValid)
-            {
-                return Json(new ResponseDto
-                {
-                    IsSuccess = false,
-                    Message = "Invalid input data."
-                });
-            }
-
-            var newLink = _mapper.Map<UserLinkViewModel>(newLinkDto);
-            newLink.Id = new Random().Next(1, 1000);
-
-            if (image != null)
-            {
-                using (var ms = new MemoryStream())
-                {
-                    image.CopyTo(ms);
-                    var fileBytes = ms.ToArray();
-                    newLink.Image = fileBytes;
-                }
-            }
-
-            var response = new ResponseDto<string>
-            {
-                IsSuccess = true,
-                Data = await _viewRenderService.RenderToStringAsync("_UserLinkRowPartial", newLink)
-            };
-
-            return Json(response);
-
-
+            return null;
         }
+
+        var imageBytes = System.IO.File.ReadAllBytes(filePath);
+        return imageBytes;
     }
 }
