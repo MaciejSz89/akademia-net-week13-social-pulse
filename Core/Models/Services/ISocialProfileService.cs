25c1f8a5f6040288a1c58f8ae6d7d0a937d30d54
Maciej Szafranski <maciej.szafranski1@gmail.com>
2025-02-24 23:35:14 +0100

    Profiles are loaded and next profiles too. Cache clear remained

--- Core/Models/Services/ISocialProfileService.cs
+++ Core/Models/Services/ISocialProfileService.cs
@@ -5,6 +5,8 @@ namespace SocialPulse.Core.Models.Services
 {
     public interface ISocialProfileService
     {
+        Task<(string sessionGuid, IEnumerable<SocialProfile> profiles)> GetInitSocialProfilesAsync(int count);
+        Task<IEnumerable<SocialProfile>> GetNextSocialProfilesAsync(string sessionGuid, int count);
         Task<SocialProfile> GetSocialProfileAsync();
         Task<SocialProfile> GetSocialProfileAsync(int id);
         Task<IEnumerable<SocialProfile>> GetSocialProfilesAsync();
---
+++ Core/ViewModels/InitSocialProfilesViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialPulse.Core.ViewModels
+{
+    public class InitSocialProfilesViewModel
+    {
+
+        public string SessionGuid { get; set; } = null!;
+        public IEnumerable<SocialProfileViewModel> SocialProfiles { get; set; } = new List<SocialProfileViewModel>();
+        
+    }
+}
